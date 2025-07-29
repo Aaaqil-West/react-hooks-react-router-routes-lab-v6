331df0010b7b1e96743b0b5c621892516200114e
aaaqil <aaaqil.west@student.moringaschool.com>
2025-07-29 17:28:47 +0300

    Implement Movie app routing, pages, and navigation

---
+++ src/components/MovieCard.js
@@ -0,0 +1,13 @@
+ased on the promptimport React from "react";
+import { Link } from "react-router-dom";
+
+function MovieCard({ movie }) {
+  return (
+    <div className="movie-card">
+      <h2>{movie.title}</h2>
+      <Link to={`/movie/${movie.id}`}>View Details</Link>
+    </div>
+  );
+}
+
+export default MovieCard;
---
+++ src/pages/Actors.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+
+function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then((r) => r.json())
+      .then(setActors);
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <article key={actor.name}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie) => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </>
+  );
+}
+
+export default Actors;
